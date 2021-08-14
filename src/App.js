173ddcb874314b148e5f2865a7652877eea59173
alinirv <alini.vicente@yahoo.com.br>
2021-08-14 19:19:23 -0300

    Consome dados de uma API
    Esse commit modifica a app com o consumo de dados de uma API,
    adição do pacote axios.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState('')
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
   return (
-    <>      
-      <p>Edit <code>src/App.js</code> and save to reload.</p>      
+    <>   
+      <input className="usuario" placeholder="Usuário"value={usuario} onChange={e => setUsuario(e.target.value)}/>    
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>    
     </>
   );
 }
