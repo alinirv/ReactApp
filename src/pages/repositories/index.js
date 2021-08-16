0959666855fcc0afac65b79066d83ea4c4ba5712
alinirv <alini.vicente@yahoo.com.br>
2021-08-15 23:37:59 -0300

    Adiciona paginas a App
    Este commit é referente a adição de paginas a App e sua organização,
     Pacotes inclusos:
     -axios
     -react-router-dom
     -styled-components

---
+++ src/pages/repositories/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Repositories(){
+    return (
+        <>
+            <h1>Repositories</h1>
+            <ul>
+                <li>Repositorio 01</li>
+                <li>Repositorio 02</li>
+                <li>Repositorio 03</li>
+                <li>Repositorio 04</li>
+                <li>Repositorio 05</li>
+                <li>Repositorio 06</li>
+                <li>Repositorio 07</li>
+                <li>Repositorio 08</li>
+                <li>Repositorio 09</li>
+                <li>Repositorio 10</li>
+            </ul>
+        </>
+    )
+}
